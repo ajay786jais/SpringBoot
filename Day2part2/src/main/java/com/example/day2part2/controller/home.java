47ddb0d7eb19c946481d66118a69b52de1e77b3d
ajay786jais <ajay.jaiswal_cs23@gla.ac.in>
2026-01-19 15:25:50 +0530

    Added 5 data

--- Day2part2/src/main/java/com/example/day2part2/controller/home.java
+++ Day2part2/src/main/java/com/example/day2part2/controller/home.java
@@ -5,11 +5,18 @@ import com.example.day2part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student =new StudentModel(1,"AJAY","ajay@gmail");
-        return student;
+    public ArrayList<StudentModel> getStudent(){
+        ArrayList<StudentModel> arr = new ArrayList<>();
+        arr.add(new StudentModel(1,"AJAY","ajay@gmail"));
+        arr.add(new StudentModel(1,"Aditya raj","adityaraj@gmail"));
+        arr.add(new StudentModel(1,"Aditya shukla","aditya@gmail"));
+        arr.add(new StudentModel(1,"Abhisek ","abhisek@gmail"));
+        arr.add(new StudentModel(1,"Agrim","agrim@gmail"));
+        return arr;
     }
 }
